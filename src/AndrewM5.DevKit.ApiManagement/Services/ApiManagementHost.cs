ea0c1c355203b07929d0e3c6b048bd5de109cf64
72CHBTS\andre <andrewjmarley03@gmail.com>
2026-04-04 17:50:47 -0500

    Actually finished the API module documentation

--- src/AndrewM5.DevKit.ApiManagement/Services/ApiManagementHost.cs
+++ src/AndrewM5.DevKit.ApiManagement/Services/ApiManagementHost.cs
@@ -3,12 +3,24 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace AndrewM5.DevKit.ApiManagement.Services;
 
+/// <summary>
+/// Provides a static entry point to access the <see cref="IApiManager"/> 
+/// This host must be initialized during application startup after the DI container is built.
+/// </summary>
 public static class ApiManagementHost
 {
     private const string NoInit = "ApiManagementHost has not been initialized.";
 
     private static IApiManager? _apiManager;
 
+    /// <summary>
+    /// Initializes the static host with the <see cref="IApiManager"/> resolved from the service provider.
+    /// </summary>
+    /// <param name="sp">The <see cref="IServiceProvider"/> containing the registered API management services.</param>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="sp"/> is null.</exception>
+    /// <exception cref="InvalidOperationException">
+    /// Thrown if <see cref="IApiManager"/> has not been registered in the service collection.
+    /// </exception>
     public static void Initialize(IServiceProvider sp)
     {
         if (sp == null)
@@ -23,6 +35,13 @@ public static class ApiManagementHost
         }
     }
 
+    /// <summary>
+    /// Gets the globally accessible instance of the <see cref="IApiManager"/>.
+    /// </summary>
+    /// <value> The initialized <see cref="IApiManager"/> instance. </value>
+    /// <exception cref="InvalidOperationException">
+    /// Thrown if <see cref="Initialize"/> was not called prior to accessing this property.
+    /// </exception>
     public static IApiManager ApiManager
     {
         get
--- src/AndrewM5.DevKit.ApiManagement/Services/ApiManagementServiceCollection.cs
+++ src/AndrewM5.DevKit.ApiManagement/Services/ApiManagementServiceCollection.cs
@@ -5,8 +5,18 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace AndrewM5.DevKit.ApiManagement.Services;
 
+/// <summary>
+/// Extension methods for setting up API management services in an <see cref="IServiceCollection"/>.
+/// </summary>
 public static class ApiManagementServiceCollection
 {
+    /// <summary>
+    /// Adds the API management infrastructure to the service collection, including configuration 
+    /// binding and the <see cref="IApiManager"/> singleton.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
+    /// <param name="configuration">The application configuration used to bind <see cref="ApiManagerSettings"/>.</param>
+    /// <returns>The original <see cref="IServiceCollection"/> for chaining calls.</returns>
     public static IServiceCollection AddApiManagement(this IServiceCollection services, IConfiguration configuration)
     {
         services.Configure<ApiManagerSettings>(configuration.GetSection("AndrewM5.DevKit:ApiManager"));
